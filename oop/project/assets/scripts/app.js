7fa422032ab94016bc06459b31950c51e93fb8b6
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-07-01 23:22:17 -0300

    split into classes

--- oop/project/assets/scripts/app.js
+++ oop/project/assets/scripts/app.js
@@ -1,50 +1,64 @@
-const productList = {
-  products: [
-    {
-      title: 'Product1',
-      imageUrl: 'https://via.placeholder.com/150',
-      price: 1.99,
-      description: 'some product'
-    },
-
-    {
-      title: 'Product2',
-      imageUrl: 'https://via.placeholder.com/150',
-      price: 1.99,
-      description: 'some product'
-    },
-
-    {
-      title: 'Product3',
-      imageUrl: 'https://via.placeholder.com/150',
-      price: 1.99,
-      description: 'some product'
-    }
-  ],
-
-  render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-
-    for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-       <div>
-         <img src="${prod.imageUrl}" alt="${prod.title}" />
-         <div class="product-item__content">
-           <h2>${prod.title}</h2>
-           <h3>\$${prod.price}</h3>
-           <p>${prod.description}</p>
-           <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
-      prodList.append(prodEl);
-    }
-    renderHook.append(prodList);
+class Product {
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
   }
 }
 
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+     <div>
+       <img src="${this.product.imageUrl}" alt="${this.product.title}" />
+       <div class="product-item__content">
+         <h2>${this.product.title}</h2>
+         <h3>\$${this.product.price}</h3>
+         <p>${this.product.description}</p>
+         <button>Add to Cart</button>
+        </div>
+      </div>
+    `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+     'Product1',
+     'https://via.placeholder.com/150',
+      1.99,
+      'some product'
+    ),
+    new Product(
+      'Product1',
+      'https://via.placeholder.com/150',
+       1.99,
+       'some product'
+     ),
+    ]
+
+    render() {
+      const renderHook = document.getElementById('app');
+      const prodList = document.createElement('ul');
+      prodList.className = 'product-list';
+  
+      for (const prod of this.products) {
+        const productItem = new ProductItem(prod);
+        const prodEl = productItem.render();
+        prodList.append(prodEl);
+      }
+      renderHook.append(prodList);
+    }
+}
+
+const productList = new ProductList();
 productList.render();
