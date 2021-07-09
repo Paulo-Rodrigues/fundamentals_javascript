e20568deb648917c235d7a04c145836f7b1b8f1c
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-07-08 21:52:35 -0300

    immutability

--- functional/concepts/immutability.js
+++ functional/concepts/immutability.js
@@ -9,3 +9,18 @@ function ordenar(array) {
   return [...array].sort()
 }
 
+const pessoa = {
+  nome: 'Maria',
+  altura: 1.76,
+  cidade: 'SP'
+}
+
+// const novaPessoa = pessoa // atribuido por referencia, dados internos mutaveis
+
+// impura
+function alteraPessoa(pessoa) {
+  const nova = { ...pessoa }
+  nova.nome = 'Joao'
+  return nova
+}
+
